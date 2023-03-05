5b2a026a1b74a9832fd34a3387f3211e95795ae3
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-03-05 09:48:54 +0900

    Add basic JSX in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Weather App</h1>
+      <h2>Display weather here</h2>
+      <button>Start fetching weather every 5 seconds</button>
+      <button>Stop fetching weather</button>
     </div>
   );
 }
